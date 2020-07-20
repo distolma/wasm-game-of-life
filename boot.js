51d5b721ab6f1ae20c5406014ec661247b5911c4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-20 20:18:13 +0000

    Deploy distolma/wasm-game-of-life to github.com/distolma/wasm-game-of-life.git:gh-pages

--- boot.js
+++ boot.js
@@ -183,7 +183,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/game_of_life_bg.wasm":"cc218ba0fb9b4444424d"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/game_of_life_bg.wasm":"8a698c6044cbb5c7063e"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
